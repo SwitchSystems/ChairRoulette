5de4d7ed953f51ebe3507a57131e3d846a4fa32c
Jon Day <jonathanmday29@gmail.com>
2015-02-19 23:00:51 +0000

    adding game api

---
+++ module/gameRpc/src/gameRpc/V1/Rpc/Game/GameController.php
@@ -0,0 +1,11 @@
+<?php
+namespace gameRpc\V1\Rpc\Game;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class GameController extends AbstractActionController
+{
+    public function gameAction()
+    {
+    }
+}
