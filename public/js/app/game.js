80fceaaf9f87fc5f6704a7d7545b778bbb6ab867
Teletubby <georgedeheer6919@gmail.com>
2015-02-19 23:40:05 +0000

    progress so far on the game board

--- public/js/app/game.js
+++ public/js/app/game.js
@@ -1,8 +1,52 @@
     "use strict";
-
 $(function() {
 	
-	// common functions
+	startRound();
 	
+	function startRound() {
+		var timeout = 10;
+		var delay = roundData['delay'];
+		
+		createChairs();
+		
+		var counter = setInterval(musicPlay, 1);
+	}
+	
+	function createChairs() {
+		
+	}
+	
+	function musicPlay() {
+		delay = delay - 1;
+		if (delay <= 0)
+	    {
+	       clearInterval(counter);
+	       musicStop();
+	       return;
+	    }
+	}
+	
+	function musicStop() {
+		$('div.chair').show();
+		$('audio').stop();
+		
+		counter = setInterval(timeoutRound, 1000);
+	}
+	
+	function timeoutRound() {
+		timeout = timeout - 1;
+		if (timeout <= 0)
+	    {
+	       clearInterval(counter);
+	       endRound();
+	       return;
+	    }
+	}
+	
+	function endRound() {
+		$('div.chair').remove();
+		
+		startRound();
+	}
 	
 });
