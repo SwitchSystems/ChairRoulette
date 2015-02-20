3b25e63c4b00f6dfb515bb137814ff09eb75f847
Teletubby <georgedeheer6919@gmail.com>
2015-02-20 01:11:30 +0000

    music can play now if we ever get there.

--- public/js/app/game.js
+++ public/js/app/game.js
@@ -1,14 +1,26 @@
     "use strict";
 $(function() {
 	
+	setInterval(fetchRoundData, 500);
+		
 	var board = $('div#game-area');
 	board.style.width = '550px';
 	board.style.height = '550px';
 	var round = '<h1 class="round">Round: ';
 	startRound();
 	
+	function fetchRoundData() {
+		'do api request to get the current round data';
+		
+		if($roundData != null) {
+			startRound();
+		}
+	}
+	
 	function startRound() {
 		$('div#gameintro').hide();
+		
+		$('#game-area').append('<audio controls><source src="game.mp3" type="audio/mpeg"></audio>');
 		
 		var timeout = 10;
 		var delay = roundData['delay'];
@@ -31,9 +43,6 @@ $(function() {
 			chairElement.style.position = "absolute";
 			chairElement.style.left = chair['x'];
 			chairElement.style.top = chair['y'];
-			chairElement.click(function() {
-				'send api request to sit with chair[id] and difference between now and time music stopped';
-			});
 			chairElement.hide();
 		}
 		
@@ -48,13 +57,12 @@ $(function() {
 		$('div.chair').click(function(event) {
 			'send api request to sit with chair[id] and difference between event.timestamp and now variable';
 		});
-		
-		counter = setTimeout(endRound, 5000);
 	}
 	
 	function endRound() {
 		$('h1.round').remove();
 		$('div.chair').remove();
+		
 	}
 	
 	function displayPlayers() {
